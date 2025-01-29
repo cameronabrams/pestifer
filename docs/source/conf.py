f05927862651fb16d30f124931590a272fc22f06
Cameron Abrams <cfa22@drexel.edu>
2025-01-29 14:54:26 -0500

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,8 +7,8 @@ project = 'pestifer'
 copyright = '2023-2024, Cameron F. Abrams'
 author = 'cfa22@drexel.edu'
 
-release = '1.4'
-version = '1.4.8'
+release = '1.6'
+version = '1.6.1'
 
 # -- General configuration
 
