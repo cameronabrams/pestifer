89e7247bfc6077687e22c0d5be7487b15a654c26
Cameron Abrams <cfa22@drexel.edu>
2025-06-06 14:56:00 -0400

    builds

--- tests/production/test_builds.py
+++ tests/production/test_builds.py
@@ -1,33 +1,20 @@
 # Author: Cameron F. Abrams, <cfa22@drexel.edu>
-# import unittest
-from pestifer.config import Config
 from pestifer.resourcemanager import ResourceManager
 import os
 import shutil
-from glob import glob
 import subprocess
 import yaml
 import pytest
-        
-def cleanup(excl_exts=['log','png','csv']):
-    excl=set()
-    for ex in excl_exts:
-        excl=excl.union(set(glob(f'*.{ex}')))
-    rmv=list(set(glob('*'))-excl)
-    for r in rmv:
-        os.remove(r)
 
 def do_it(exnumber):
     nstr=f'{exnumber:02d}'
+    if os.path.exists(f'__test_build_{nstr}'):
+        shutil.rmtree(f'__test_build_{nstr}')
+    os.mkdir(f'__test_build_{nstr}')
+    os.chdir(f'__test_build_{nstr}')
     RM=ResourceManager()
     configfile=RM.get_example_yaml_by_index(exnumber)
     configfile_basename=os.path.basename(configfile)
-    d=str(exnumber)
-    if os.path.exists(d):
-        shutil.rmtree(d)
-    os.mkdir(d)
-    os.chdir(d)
-    shutil.copy(configfile,f'./{configfile_basename}')
     with open(configfile_basename,'r') as f:
         specs=yaml.safe_load(f)
     prod_basename=specs['tasks'][-1]['terminate']['package']['basename']
@@ -37,9 +24,7 @@ def do_it(exnumber):
     with open('console.log','w') as f:
         f.write(res.stdout)
     assert(os.path.exists(prod_tgz))
-        # end_message=res.stderr.split('\n')[-2]
-        # assert(end_message=='INFO> pestifer runtime ends.')
-    cleanup()
+    os.chdir('..')
 
 @pytest.mark.slow
 def test_example_build01():
